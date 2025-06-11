451f0926e390841ada03320282fdaf84701858bc
Pegusto05 <yoy862820@gmail.com>
2025-06-11 05:18:07 +0000

    completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,56 +1,103 @@
 #include <stdio.h>
 
 /*
-Desafio Batalha Naval - Nível Aventureiro
+Desafio Batalha Naval - Nível Mestre
 
 Objetivo:
-- Expandir o tabuleiro para 10x10.
-- Posicionar quatro navios: dois na vertical/horizontal e dois na diagonal.
-- Usar o valor 3 para marcar as posições ocupadas no tabuleiro.
-- Exibir o tabuleiro inteiro, mostrando 0 para posições livres e 3 para ocupadas.
+- Implementar habilidades especiais representadas por padrões em matrizes.
+- Usar loops aninhados para percorrer e preencher as áreas atingidas.
+- Três padrões: cone, cruz e octaedro.
+- Exibir as matrizes mostrando 0 para não afetadas e 1 para áreas atingidas.
 
 Novidades:
-- Trabalho com matriz maior.
-- Posicionamento de navios na diagonal, que exige controle cuidadoso dos índices.
-- Impressão completa do tabuleiro para visualização clara do posicionamento.
+- Manipulação de matrizes para representar áreas de efeito.
+- Uso de loops aninhados para preencher padrões complexos.
+- Impressão clara dos padrões para fácil visualização.
 */
 
-// Definindo tamanho do tabuleiro 10x10
-#define LINHAS 10
-#define COLUNAS 10
+#define TAM 5  // Tamanho das matrizes de habilidade
 
-int main() {
-    int tabuleiro[LINHAS][COLUNAS] = {0}; // Inicializa tudo com zero
-
-    // Posicionando navio vertical (tamanho 4) na coluna 2, linhas 1 a 4
-    for (int i = 1; i <= 4; i++) {
-        tabuleiro[i][2] = 3;
-    }
-
-    // Posicionando navio horizontal (tamanho 5) na linha 6, colunas 3 a 7
-    for (int j = 3; j <= 7; j++) {
-        tabuleiro[6][j] = 3;
-    }
-
-    // Posicionando navio diagonal (tamanho 3) a partir da posição (2,5) descendo para a direita
-    for (int k = 0; k < 3; k++) {
-        tabuleiro[2 + k][5 + k] = 3;
-    }
-
-    // Posicionando outro navio diagonal (tamanho 4) a partir da posição (7,1) subindo para a direita
-    // Como sobe, linha diminui e coluna aumenta
-    for (int k = 0; k < 4; k++) {
-        tabuleiro[7 - k][1 + k] = 3;
-    }
-
-    // Exibindo o tabuleiro completo
-    printf("Tabuleiro completo (0 = livre, 3 = navio):\n\n");
-    for (int i = 0; i < LINHAS; i++) {
-        for (int j = 0; j < COLUNAS; j++) {
-            printf("%d ", tabuleiro[i][j]);
+// Função para imprimir uma matriz
+void imprimirMatriz(int matriz[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+int main() {
+    int cone[TAM][TAM] = {0};
+    int cruz[TAM][TAM] = {0};
+    int octaedro[TAM][TAM] = {0};
+
+    // Preenchendo padrão CONE
+    /*
+    Exemplo do padrão cone:
+    0 0 1 0 0
+    0 1 1 1 0
+    1 1 1 1 1
+    */
+    // Para facilitar, vamos preencher a parte inferior da matriz centralizada.
+    for (int i = 2; i < TAM; i++) {
+        int inicio = 2 - (i - 2);
+        int fim = 2 + (i - 2);
+        for (int j = inicio; j <= fim; j++) {
+            cone[i][j] = 1;
+        }
+    }
+
+    // Preenchendo padrão CRUZ
+    /*
+    Exemplo do padrão cruz:
+    0 0 1 0 0
+    1 1 1 1 1
+    0 0 1 0 0
+    */
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            // Linha do meio e coluna do meio preenchidas
+            if (i == 2 || j == 2) {
+                cruz[i][j] = 1;
+            }
+        }
+    }
+
+    // Preenchendo padrão OCTAEDRO
+    /*
+    Exemplo do padrão octaedro:
+    0 0 1 0 0
+    0 1 1 1 0
+    0 0 1 0 0
+    */
+    // Preenchendo uma espécie de "diamante" no meio
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 2 - (i - 1); j <= 2 + (i - 1); j++) {
+            octaedro[i][j] = 1;
+        }
+    }
+    // Linha do meio completa no meio do octaedro
+    for (int j = 2; j <= 2; j++) {
+        octaedro[2][j] = 1;
+    }
+    // Parte inferior igual a parte superior (simetria)
+    for (int i = 3; i < 4; i++) {
+        for (int j = 2 - (4 - i - 1); j <= 2 + (4 - i - 1); j++) {
+            octaedro[i][j] = 1;
+        }
+    }
+
+    // Imprimindo os padrões
+    printf("Habilidade - Cone:\n");
+    imprimirMatriz(cone);
+
+    printf("Habilidade - Cruz:\n");
+    imprimirMatriz(cruz);
+
+    printf("Habilidade - Octaedro:\n");
+    imprimirMatriz(octaedro);
 
     return 0;
 }
